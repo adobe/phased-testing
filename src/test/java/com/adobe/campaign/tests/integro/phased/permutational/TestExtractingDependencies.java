8ce7edb1efe0c976f1bb0164a49416ed1ae9c117
baubakg <development@gandomi.com>
2022-10-19 16:48:47 +0200

    added more tests

--- src/test/java/com/adobe/campaign/tests/integro/phased/permutational/TestExtractingDependencies.java
+++ src/test/java/com/adobe/campaign/tests/integro/phased/permutational/TestExtractingDependencies.java
@@ -11,7 +11,9 @@
  */
 package com.adobe.campaign.tests.integro.phased.permutational;
 
+import com.adobe.campaign.tests.integro.phased.data.permutational.MultipleProducerConsumer;
 import com.adobe.campaign.tests.integro.phased.data.permutational.SimpleProducerConsumer;
+import com.adobe.campaign.tests.integro.phased.data.permutational.SimpleProducerConsumerStaticImport;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
@@ -23,7 +25,7 @@ import static org.hamcrest.Matchers.*;
 public class TestExtractingDependencies {
 
     @Test
-    public void testFetchProduceConsume()
+    public void testFetchExtractingProduceConsume()
             throws NoSuchMethodException, SecurityException, IOException {
 
         Class<SimpleProducerConsumer> l_testClass = SimpleProducerConsumer.class;
@@ -43,12 +45,78 @@ public class TestExtractingDependencies {
         assertThat("we should set the correct consume", dependencies.getStep("bbbbb").getConsumeSet(), hasSize(0));
         assertThat("we should set the correct consume", dependencies.getStep("bbbbb").getProduceSet(),
                 hasItems("bbbbkey"));
-        assertThat("we should set the correct consume", dependencies.getStep("bbbbb").getStepPointer(), equalTo(18));
 
         assertThat("we should set the correct consume", dependencies.getStep("aaaa").getProduceSet(), hasSize(0));
         assertThat("we should set the correct consume", dependencies.getStep("aaaa").getConsumeSet(),
                 hasItems("bbbbkey"));
-        assertThat("we should set the correct consume", dependencies.getStep("aaaa").getStepPointer(), equalTo(22));
+
+        assertThat("The pointer of aaaa should be after that of bbbbb", dependencies.getStep("aaaa").getStepPointer(),
+                greaterThan(dependencies.getStep("bbbbb").getStepPointer()));
+    }
+
+    @Test
+    public void testFetchExtractingProduceConsumeStaticImports()
+            throws NoSuchMethodException, SecurityException, IOException {
+
+        Class<SimpleProducerConsumerStaticImport> l_testClass = SimpleProducerConsumerStaticImport.class;
+
+        ScenarioStepDependencies dependencies = ScenarioStepDependencyFactory.listMethodCalls(l_testClass);
+
+        assertThat("Our object should have a map od StpDependencies", dependencies.getStepDependencies(),
+                instanceOf(Map.class));
+
+        assertThat("Our object should be linked to the analyzed class", dependencies.getScenarioName(),
+                equalTo(SimpleProducerConsumerStaticImport.class.getTypeName()));
+
+        assertThat("We should now have two steps defined here", dependencies.getStepDependencies().keySet().size(),
+                equalTo(2));
+
+        assertThat("We should have fetched the correct methods", dependencies.getStepDependencies().keySet(),
+                containsInAnyOrder("bbbbb", "aaaa"));
+
+        assertThat("we should set the correct consume", dependencies.getStep("bbbbb").getConsumeSet(), hasSize(0));
+        assertThat("we should set the correct consume", dependencies.getStep("bbbbb").getProduceSet(),
+                hasItems("bbbbkey"));
+
+        assertThat("we should set the correct consume", dependencies.getStep("aaaa").getProduceSet(), hasSize(0));
+        assertThat("we should set the correct consume", dependencies.getStep("aaaa").getConsumeSet(),
+                hasItems("bbbbkey"));
+
+        assertThat("The pointer of aaaa should be after that of bbbbb", dependencies.getStep("aaaa").getStepPointer(),
+                greaterThan(dependencies.getStep("bbbbb").getStepPointer()));
+    }
+
+    @Test
+    public void testFetchExtractingMultipleProduceConsumeStaticImports()
+            throws NoSuchMethodException, SecurityException, IOException {
+
+        Class<MultipleProducerConsumer> l_testClass = MultipleProducerConsumer.class;
+
+        ScenarioStepDependencies dependencies = ScenarioStepDependencyFactory.listMethodCalls(l_testClass);
+
+        assertThat("We should now have two steps defined here", dependencies.getStepDependencies().keySet().size(),
+                equalTo(3));
+
+        assertThat("We should have fetched the correct methods", dependencies.getStepDependencies().keySet(),
+                containsInAnyOrder("bbbbb", "aaaaa", "ccccc"));
+
+        assertThat("we should set the correct consume", dependencies.getStep("ccccc").getConsumeSet(), hasSize(0));
+        assertThat("we should set the correct consume", dependencies.getStep("ccccc").getProduceSet(),
+                hasItems("keyA"));
+
+        assertThat("we should set the correct consume", dependencies.getStep("bbbbb").getConsumeSet(), hasSize(0));
+        assertThat("we should set the correct consume", dependencies.getStep("bbbbb").getProduceSet(),
+                hasItems("keyB"));
+
+        assertThat("we should set the correct consume", dependencies.getStep("aaaaa").getProduceSet(), hasSize(0));
+        assertThat("we should set the correct consume", dependencies.getStep("aaaaa").getConsumeSet(),
+                hasItems("keyA","keyB"));
+
+        assertThat("The pointer of aaaaa should be after that of bbbbb", dependencies.getStep("aaaaa").getStepPointer(),
+                greaterThan(dependencies.getStep("bbbbb").getStepPointer()));
+
+        assertThat("The pointer of bbbbb should be after that of ccccc", dependencies.getStep("bbbbb").getStepPointer(),
+                greaterThan(dependencies.getStep("ccccc").getStepPointer()));
     }
 
 }
